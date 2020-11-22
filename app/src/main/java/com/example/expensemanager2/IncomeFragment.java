975b7cf7d7cc125a03819682f39b7352e94c5f25
Neha <nehu19102000@gmail.com>
2020-11-22 06:12:15 +0530

    21 final done

--- app/src/main/java/com/example/expensemanager2/IncomeFragment.java
+++ app/src/main/java/com/example/expensemanager2/IncomeFragment.java
@@ -2,11 +2,21 @@ package com.example.expensemanager2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.expensemanager2.Model.Data;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +65,10 @@ public class IncomeFragment extends Fragment {
         }
     }
 
+    //Firebase database
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_income, container, false);
+
     }
-}
